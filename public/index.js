2f3dcca738d4e0d03e5b5c0ca3eb8def4c2bf296
Bebe-Flo-So <120537192+Bebe-Flo-So@users.noreply.github.com>
2023-03-30 18:14:18 -0400

    need to add charts

--- public/index.js
+++ public/index.js
@@ -1,8 +1,51 @@
+let highestValue = []
+
 async function main() {
 
     const timeChartCanvas = document.querySelector('#time-chart');
     const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
     const averagePriceChartCanvas = document.querySelector('#average-price-chart');
+
+    const response = await fetch('')
+    const data = await response.json()
+
+let GME = data.GME
+let MSFT = data.MSFT
+let DIS = data.DIS
+let BTNX = data.BTNX
+
+const stocks = [GME, MSFT, DIS, BTNX];
+stocks.forEach(stock => stock.values.reverse())
+
+new Chart(timeChartCanvas.getContext('2d'), {
+    type: 'line',
+    data: {
+        labels: stocks[0].values.map(value => value.datetime),
+        datasets: stocks.map( stock => ({
+            label: stock.meta.symbol,
+            data: stock.values.map(value => parseFloat(value.high)),
+            backgroundColor:  getColor(stock.meta.symbol),
+            borderColor: getColor(stock.meta.symbol),
+        }))
+    }
+});
+
+function getColor(stock){
+    if(stock === "GME"){
+        return 'rgba(61, 161, 61, 0.7)'
+    }
+    if(stock === "MSFT"){
+        return 'rgba(209, 4, 25, 0.7)'
+    }
+    if(stock === "DIS"){
+        return 'rgba(18, 4, 209, 0.7)'
+    }
+    if(stock === "BNTX"){
+        return 'rgba(166, 43, 158, 0.7)'
+    }
+}
+
+new Chart
 
 }
 
